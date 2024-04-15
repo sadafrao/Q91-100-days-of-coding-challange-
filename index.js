0658916db05adef18d50b56cf9f76c09c46c6e11
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:32:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Q91
+// Adding .push array method
+var friends = ["sana", "sadia", "sahir"];
+friends.push("Ahmed");
+console.log(friends);
---
+++ index.ts
@@ -0,0 +1,5 @@
+// Q91
+// Adding .push array method
+let friends : string[] = ["sana","sadia","sahir"];
+friends.push("Ahmed")
+console.log(friends);
